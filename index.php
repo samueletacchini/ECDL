6fd921644b5986db825af0deb69a58884aac51be
samueletacchini <tacchinisamuele@gmail.com>
2018-03-20 10:52:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ and open the template in the editor.
     <body>
         <?php
         // put your code here
-        echo "CIAOO LOL";
+        echo "CIAOO LOL HAHAH";
         ?>
     </body>
 </html>
