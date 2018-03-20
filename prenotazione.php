95ab483747cb72b32ae622285ec826807bf296ab
Nicolas Capogrosso <Nicolas Capogrosso@DESKTOP-7VBJFIM>
2018-03-20 12:59:39 +0100

    Pagina Di Prenotazione Completata

--- prenotazione.php
+++ prenotazione.php
@@ -5,15 +5,19 @@
     </head>
     <body>
         
-        <h1 align="center"> PRENOTAZIONE ESAME </h1>     
+        <fieldset>
+        <h1 align="center"> PRENOTAZIONE ESAME </h1>   
+        </fieldset>
+        
         <form name=”casellaTesto” method=”post”>
-            <p> Skill Card N. : </p>
+             <fieldset>
+            <p> Skill Card N. :
           <input type=”numebr” name=”card” size=”20″ maxlength=”20″>
-            <p> Cognome : </p>
+            <p> Cognome :
           <input type=”text” name=”cognome” size=”20″ maxlength=”20″>
-            </br><p> Nome : </p>
+            </br><p> Nome : 
           <input type=”text” name=”nome” size=”20″ maxlength=”20″>
-            </br><p> Data Di Nascita : </p>
+            </br><p> Data Di Nascita : 
             <select name = "Giorno">
 	      <option>Giorno</option>
 	      <?php
@@ -39,10 +43,57 @@
             <select>
               <option>Anno</option>
 	      <?php
-		for($year = 1900; $year <= 2030;$year++)
+		for($year = Date('Y'); $year >= 1900 ;$year--)
 		echo"<option value = '".$year."'>".$year."</option>";
 	      ?>
             </select>
+            <p>Città :
+            <input type=”text” name=”cit” size=”20″ maxlength=”20″>
+            CAP :
+            <input type=”text” name=”cap” size=”20″ maxlength=”20″>
+            Provincia : 
+            <input type=”text” name=”provincia” size=”20″ maxlength=”20″>
+            <p> Recapito Telefonico :
+            <input type=”number” name=”telefono” size=”20″ maxlength=”20″>
+             <p> Indirizzo E-Mail :
+            <input type=”text” name=”mail” size=”20″ maxlength=”20″>
+            
+             <br><h3>Tipologie Di Candidati :</h3>
+             
+                 <input type="checkbox" name="docente" value="html"/> Docente/ATA
+                 <br><input type="checkbox" name="personale" value="css"/> Personale Corpi Militari ed Enti Ministeriali convenzionati 
+                 <br><input type="checkbox" name="studente" value="javascript"/> Studente sup. : 
+                 
+                 Scuola :
+                     <input type=”text” name=”Scuola” size=”20″ maxlength=”20″>
+                 Classe :
+                     <input type=”text” name=”Classe” size=”20″ maxlength=”20″>
+                 pecializzazione :
+                     <input type=”text” name=”Secializzazione” size=”20″ maxlength=”20″>
+            
+                 <br><input type="checkbox" name="esterno" value="javascript"/> Esterni
+                 
+              <br><h3>Barrare con una Spunta uno o più moduli per i quali si vuole sostenere l' esame :</h3>
+              
+               <input type="checkbox" name="modulo1" value="modulo1"/> Modulo 1 : Computer Essentials
+                 <br><input type="checkbox" name="modulo2" value="modulo2"/> Modulo 2 : Online Essentials
+                 <br><input type="checkbox" name="modulo3" value="modulo3"/> Modulo 3 : Word Processing
+                 <br><input type="checkbox" name="modulo4" value="modulo4"/> Modulo 4 : Spreadsheets
+                 <br><input type="checkbox" name="modulo5" value="modulo5"/> Modulo 5 : IT Security
+                 <br><input type="checkbox" name="modulo6" value="modulo6"/> Modulo 6 : Presentation
+                 <br><input type="checkbox" name="modulo7" value="modulo7"/> Modulo 7 : Online Collaboration
+                 
+                 <br><h3>Data Prenotazione Esame :</h3>
+                
+                     <table>
+                         <tr><td> Data </td><td> Ora</td></tr>
+                         <tr><td><input type="checkbox" name="data1" value="modulo1"/> Giovedì 22 Marzo 2018</td><td>17:00 - 19:30</td></tr>
+                         <tr><td><input type="checkbox" name="data2" value="modulo2"/> Giovedì 19 Aprile 2018</td><td>15:00 - 17:30</td></tr>
+                         <tr><td><input type="checkbox" name="data3" value="modulo3"/> Giovedì 24 Maggio 2018 </td><td>	17:00 - 19:30</td></tr>
+                     </table>
+                 </table>
+                     
+             </fieldset>   
         </form>
        <?php
        ?>
