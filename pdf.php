12d14179884894bfe428be636ee6d3ec7f8932a4
spatecca <tacchinisamuele@gmail.com>
2018-04-11 16:46:41 +0200

    if skill card

--- pdf.php
+++ pdf.php
@@ -6,46 +6,23 @@ use setasign\Fpdi\PdfReader;
 require('fpdf17/fpdf.php');
 require_once("ConnessioneDb.php");
 $db = new ConnessioneDb();
-$id = "tccsml0257blew99";
-
-if (isset($_REQUEST["id"]) && isset($_REQUEST["type"])) {
-    $id = $_REQUEST["id"];
-    $type = $_REQUEST["type"];
-}
-
-//FIX ME like this just fot testing purposes
-$tipo = "studente";
 $datenow = date("d/m/y");
 
-$query = "SELECT * FROM `user` WHERE `codice_fiscale` = '$id'";
-$ris = $db->query($query);
+//HUST FR TESTINGG!!!
+$tipo = "studente";
 
-$riga = $ris->fetch_array();
-if ($riga["codice_fiscale"] != "") {
 
-    $skillcard = $riga['skill_card'];
-    $codicefiscale = $riga['codice_fiscale'];
-    $sesso = $riga['sesso'];
-    $cognome = $riga['cognome'];
-    $nome = $riga['nome'];
-    $data = $riga['data_nascita'];
-    $lnascita = $riga['luogo_nascita'];
-    $statocivile = $riga['stato_civile'];
-    $indirizzo = $riga['indirizzo'];
-    $stato = $riga['stato'];
-    $citta = $riga['citta'];
-    $cap = $riga['cap'];
-    $provincia = $riga['provincia'];
-    $telefono = $riga['telefono'];
-    $cellulare = $riga['cellulare'];
-    $mail = $riga['email'];
-    $occupazione = $riga['occupazione'];
-    $tipo = $riga['tipo'];
-
-    if (substr($tipo, 0, 8) == "studente") {
-        $scuola = substr($tipo, 10);
-        $tipo = substr($tipo, 0, 8);
+if (isset($_REQUEST["id"]) && isset($_REQUEST["type"])) {
+    if (isset($_REQUEST["idprenota"])) {
+        $id = $_REQUEST["id"];
+        $type = $_REQUEST["type"];
+        $idprenotazione = $_REQUEST["idprenota"];
+    } else {
+        $id = $_REQUEST["id"];
+        $type = $_REQUEST["type"];
     }
+
+
 
 
 
@@ -55,69 +32,101 @@ if ($riga["codice_fiscale"] != "") {
 
     switch ($type) {
         case "skillcard":
+
+            $query = "SELECT * FROM `user` WHERE `codice_fiscale` = '$id'";
+            $ris = $db->query($query);
+
+            $riga = $ris->fetch_array();
+            if ($riga["codice_fiscale"] != "") {
+
+                $skillcard = $riga['skill_card'];
+                $codicefiscale = $riga['codice_fiscale'];
+                $sesso = $riga['sesso'];
+                $cognome = $riga['cognome'];
+                $nome = $riga['nome'];
+                $data = $riga['data_nascita'];
+                $lnascita = $riga['luogo_nascita'];
+                $statocivile = $riga['stato_civile'];
+                $indirizzo = $riga['indirizzo'];
+                $stato = $riga['stato'];
+                $citta = $riga['citta'];
+                $cap = $riga['cap'];
+                $provincia = $riga['provincia'];
+                $telefono = $riga['telefono'];
+                $cellulare = $riga['cellulare'];
+                $mail = $riga['email'];
+                $occupazione = $riga['occupazione'];
+                $tipo = $riga['tipo'];
+
+                if (substr($tipo, 0, 8) == "studente") {
+                    $scuola = substr($tipo, 10);
+                    $tipo = substr($tipo, 0, 8);
+                }
+            } else {
+                echo "UTENTE NON TROVATO!";
+            }
             $pdf->Image('pdf/skillcard.png', 0, 0, 210, 297);
             //n skill card
             $pdf->SetXY(39, 58);
             $pdf->Write(0, "$skillcard");
-//rilasciata il
+            //rilasciata il
             $pdf->SetXY(98, 57.5);
             $pdf->Write(0, "$datenow");
-//codice fiscale
+            //codice fiscale
             $pdf->SetXY(42, 69);
             $pdf->Write(0, "$codicefiscale");
-//sesso
+            //sesso
             $pdf->SetXY(140, 69);
             $pdf->Write(0, "$sesso");
-//cognome
+            //cognome
             $pdf->SetXY(42, 77);
             $pdf->Write(0, "$cognome");
-//data di nascita
+            //data di nascita
             $pdf->SetXY(140, 77.5);
             $pdf->Write(0, "$data");
-//nome
+            //nome
             $pdf->SetXY(42, 86);
             $pdf->Write(0, "$nome");
-//luogo di nascita
+            //luogo di nascita
             $pdf->SetXY(140, 86);
             $pdf->Write(0, "$lnascita");
-//stato civile
+            //stato civile
             $pdf->SetXY(128, 94.5);
             $pdf->Write(0, "$statocivile");
-//indirizzo
+            //indirizzo
             $pdf->SetXY(42, 103);
             $pdf->Write(0, "$indirizzo");
-//cap
+            //cap
             $pdf->SetXY(158, 103);
             $pdf->Write(0, "$cap");
-//provincia
+            //provincia
             $pdf->SetXY(42, 111.5);
             $pdf->Write(0, "$provincia");
-//città
+            //città
             $pdf->SetXY(97, 111.5);
             $pdf->Write(0, "$citta");
-//stato
+            //stato
             $pdf->SetXY(158, 111.5);
             $pdf->Write(0, "$stato");
-//tel
+            //tel
             $pdf->SetXY(39, 120);
             $pdf->Write(0, "$telefono");
-//email
+            //email
             $pdf->SetXY(97, 120);
             $pdf->Write(0, "$mail");
-//cellulare
+            //cellulare
             $pdf->SetXY(97, 128.5);
             $pdf->Write(0, "$cellulare");
-//scolarita
+            //scolarita
             $pdf->SetXY(41.5, 145.7);
             $pdf->Write(0, "?????????");
-//occupazione
+            //occupazione
             $pdf->SetXY(140, 145.5);
             $pdf->Write(0, "$occupazione");
 
 
             switch ($tipo) {
                 case "studente":
-
                     //scuola e classe
                     $pdf->SetXY(97, 154.2);
                     $pdf->Write(0, "$scuola");
@@ -125,9 +134,6 @@ if ($riga["codice_fiscale"] != "") {
                     //studenti
                     $pdf->SetXY(38.2, 172);
                     $pdf->Write(0, "x");
-
-
-
                     break;
                 case "docenti":
                     //docenti
@@ -144,79 +150,131 @@ if ($riga["codice_fiscale"] != "") {
                     $pdf->SetXY(38.2, 193.7);
                     $pdf->Write(0, "x");
             }
-
-
-
-//modena DATA
+            //modena DATA
             $pdf->SetXY(39, 215);
             $pdf->Write(0, $datenow);
             break;
 
         case "prenotazione": {
+                $query = "SELECT user.*, prenotazione.esami, sessioni.data, sessioni.ora_da, sessioni.ora_a FROM `user`JOIN prenotazione ON prenotazione.ID_codice_fiscale = user.codice_fiscale JOIN sessioni ON sessioni.ID = prenotazione.ID_sessione WHERE user.codice_fiscale = '$id' && prenotazione.ID = $idprenotazione";
+                $ris = $db->query($query);
+
+                $riga = $ris->fetch_array();
+                if ($riga["codice_fiscale"] != "") {
+
+                    //data da user
+                    $skillcard = $riga['skill_card'];
+                    $codicefiscale = $riga['codice_fiscale'];
+                    $sesso = $riga['sesso'];
+                    $cognome = $riga['cognome'];
+                    $nome = $riga['nome'];
+                    $data = $riga['data_nascita'];
+                    $lnascita = $riga['luogo_nascita'];
+                    $statocivile = $riga['stato_civile'];
+                    $indirizzo = $riga['indirizzo'];
+                    $stato = $riga['stato'];
+                    $citta = $riga['citta'];
+                    $cap = $riga['cap'];
+                    $provincia = $riga['provincia'];
+                    $telefono = $riga['telefono'];
+                    $cellulare = $riga['cellulare'];
+                    $mail = $riga['email'];
+                    $occupazione = $riga['occupazione'];
+                    $tipo = $riga['tipo'];
+                    if (substr($tipo, 0, 8) == "studente") {
+                        $scuola = substr($tipo, 10);
+                        $tipo = substr($tipo, 0, 8);
+                    }
+
+                    //data da prenotazione
+                    $esami = $riga['esami'];
+                    $tipo = $riga['tipo'];
+
+                    //data da sessioni
+                    $dataesame = $riga['data'];
+                    $ora_da = $riga['ora_da'];
+                    $ora_a = $riga['ora_a'];
+                } else {
+                    echo "PRENOTAZIONE NON TROVATO!";
+                }
+
+
+
+
+
                 $pdf->Image('pdf/prenotazione full.png', 0, 0, 210, 297);
 
                 //n skill card
                 $pdf->SetXY(48.5, 68);
-                $pdf->Write(0, "98993");
+                $pdf->Write(0, "$skillcard");
 
-//cognome
+                //cognome
                 $pdf->SetXY(124.5, 77);
-                $pdf->Write(0, "cognome");
+                $pdf->Write(0, "$cognome");
 
-//data di nascita
+                //data di nascita
                 $pdf->SetXY(48.5, 86);
-                $pdf->Write(0, "data di nascita");
+                $pdf->Write(0, "$data");
 
-//nome
+                //nome
                 $pdf->SetXY(48.5, 77);
-                $pdf->Write(0, "nome");
+                $pdf->Write(0, "$nome");
 
-//luogo di nascita
+                //luogo di nascita
                 $pdf->SetXY(124.5, 86);
-                $pdf->Write(0, "luogo di nascita");
+                $pdf->Write(0, "$lnascita");
 
-//indirizzo
+                //indirizzo
                 $pdf->SetXY(48.5, 95.3);
-                $pdf->Write(0, "indirizzo");
+                $pdf->Write(0, "$indirizzo");
 
-//cap
+                //cap
                 $pdf->SetXY(125, 104);
-                $pdf->Write(0, "cap");
+                $pdf->Write(0, "$cap");
 
-//provincia
+                //provincia
                 $pdf->SetXY(160, 104);
-                $pdf->Write(0, "provincia");
+                $pdf->Write(0, "$provincia");
 
-//città
+                //città
                 $pdf->SetXY(48.5, 104.5);
-                $pdf->Write(0, "citta");
+                $pdf->Write(0, "$citta");
 
-//tel
+                //tel
                 $pdf->SetXY(48.5, 113.5);
-                $pdf->Write(0, "tel");
+                $pdf->Write(0, "$telefono");
 
-//email
+                //email
                 $pdf->SetXY(125, 113.5);
-                $pdf->Write(0, "email");
+                $pdf->Write(0, "$mail");
 
 
-                switch ($tipo) {
+
+
+                switch (substr($tipo, 0, 8)) {
                     case "studente":
+
+                        $classe = substr($tipo, 10, 2);
+                        $specializzazione = substr(explode(',', "$tipo")[0], 13);
+                        $scuola = explode(',', $tipo)[1];
+
+                        $tipo = substr($tipo, 0, 8);
+
                         //studente
                         $pdf->SetXY(48.7, 136.5);
                         $pdf->Write(0, "X");
 
                         //scuola
-                        $pdf->SetXY(77, 136.5);
-                        $pdf->Write(0, "scuola");
+                        $pdf->SetXY(76, 136.5);
+                        $pdf->Write(0, "$scuola");
 
                         //classe
                         $pdf->SetXY(77, 142);
-                        $pdf->Write(0, "classe");
+                        $pdf->Write(0, "$classe");
 
                         //specializzazione
                         $pdf->SetXY(124.5, 142);
-                        $pdf->Write(0, "specializzazione");
+                        $pdf->Write(0, "$specializzazione");
                         break;
                     case "docenti":
                         //docenti
@@ -234,13 +292,16 @@ if ($riga["codice_fiscale"] != "") {
                         $pdf->Write(0, "X");
                 }
 
-                $prova = "6351";
+                //data esami
+                $pdf->SetXY(77, 219);
+                $pdf->Write(0, "$dataesame, dalle $ora_da alle $ora_a");
+
 
                 //esami da prenotare
-                for ($i = 0; $i < strlen($prova); $i++) {
-                    $prova[$i] = $prova[$i] - 1;
+                for ($i = 0; $i < strlen($esami); $i++) {
+                    $esami[$i] = $esami[$i] - 1;
 
-                    $pdf->SetXY(48.7, (165.5 + ($prova[$i] * 7.1)));
+                    $pdf->SetXY(48.7, (165.5 + ($esami[$i] * 7.1)));
                     $pdf->Write(0, "X");
                 }
 
@@ -254,7 +315,7 @@ if ($riga["codice_fiscale"] != "") {
 //$pdf->Output("D", "nomefile.pdf");
     $pdf->Output();
 } else {
-    echo "CODICE FISCALE NON TROVATO!";
+    echo "effettuare la richieste fornendo un id e un tipo di PDF";
 }
 ?>
 
