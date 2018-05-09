f3bc401802a43a3a4620344508860d71d6bd64e9
Nicolas Capogrosso <Nicolas Capogrosso@DESKTOP-7VBJFIM>
2018-05-09 11:15:55 +0200

    Merge origin/master
    Conflicts:
    	index.php

---
+++ prova.php
@@ -0,0 +1,12 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+    <title></title>
+    <script type="text/javascript" src="http://code.jquery.com/jquery-1.4.4.min.js"></script>
+
+
+    
+</head>
+<body>
+    <a href="#dialog" id="modal" name="modal">Change Password</a>
+</body>
+</html>
